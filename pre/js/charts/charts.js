2f537fcc2d57390f6d47ac6e2061d07eaca3ee8c
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-21 16:29:24 +0200

    Primer desarrollo serio terminado

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,23 +1,15 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-//import { numberWithCommas2 } from './helpers';
-//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { numberWithCommas3 } from '../helpers';
+import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42', 
-COLOR_ANAG_1 = '#D1834F', 
-COLOR_ANAG_2 = '#BF2727', 
-COLOR_COMP_1 = '#528FAD', 
-COLOR_COMP_2 = '#AADCE0', 
-COLOR_GREY_1 = '#B5ABA4', 
-COLOR_GREY_2 = '#64605A', 
-COLOR_OTHER_1 = '#B58753', 
-COLOR_OTHER_2 = '#731854';
+const COLOR_PRIMARY_1 = '#F8B05C';
+let tooltip = d3.select('#tooltip');
 
 export function initChart(iframe) {
     //Lectura de datos
@@ -43,36 +35,79 @@ export function initChart(iframe) {
             .domain([0, 40])
             .range([ 0, width]);
 
+        let xAxis = function(svg) {
+            svg.call(d3.axisBottom(x).ticks(4));
+            svg.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('class', function(d,i) {
+                        if (d == 0) {
+                            return 'line-special';
+                        }
+                    })
+                    .attr('y1', '0')
+                    .attr('y2', `-${height}`)
+            });
+        }
+
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            .call(xAxis);
 
         let y = d3.scaleBand()
-                .range([ 0, height ])
-                .domain(data.map(function(d) { return d.tipo_contribucion; }));
+            .range([ 0, height ])
+            .domain(data.map(function(d) { return d.tipo_contribucion; }));
+
+        let yAxis = function(svg) {
+            svg.call(d3.axisLeft(y));
+            svg.call(function(g){g.selectAll('.domain').remove()});
+            svg.call(function(g){g.selectAll('.tick line').remove()});
+            svg.selectAll('.tick text')
+            .call(wrap, 130);
+        }
 
         svg.append("g")
-            .call(d3.axisLeft(y))
-            .selectAll('.tick text')
-            .call(wrap, 130);
-
-
+            .call(yAxis);
+        
         function init() {
             svg.selectAll("bars")
                 .data(data)
                 .enter()
                 .append("rect")
-                .attr('class','bars')
+                .attr('class','rect')
                 .attr("x", x(0) )
                 .attr("y", function(d) { return y(d.tipo_contribucion) + y.bandwidth() / 4; })
                 .attr("width", function(d) { return x(0); })
                 .attr("height", y.bandwidth() / 2 )
-                .attr("fill", function(d) {
-                    if (d.CODAUTO != 20) {
-                        return '#F8B05C';
-                    } else {
-                        return '#BF2727';
-                    }
+                .attr("fill", COLOR_PRIMARY_1)
+                .on('mouseover', function(d,i,e) {
+                    //Opacidad de las barras
+                    let bars = svg.selectAll('.rect');  
+                    bars.each(function() {
+                        this.style.opacity = '0.4';
+                    });
+                    this.style.opacity = '1';
+
+                    console.log(d);
+
+                    //Texto
+                    let html = '<p class="chart__tooltip--title">' + d.tipo_contribucion + '</p>' + 
+                    '<p class="chart__tooltip--text">Un <b>' + numberWithCommas3(parseFloat(d.valor_contribucion).toFixed(1)) + '%</b> de abuelos y abuelas contribuye con esta forma</p>';
+            
+                    tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Quitamos los estilos de la línea
+                    let bars = svg.selectAll('.rect');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+                
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip);
                 })
                 .transition()
                 .duration(2000)
@@ -80,7 +115,7 @@ export function initChart(iframe) {
         }
 
         function animateChart() {
-            svg.selectAll(".bars")
+            svg.selectAll(".rect")
                 .attr("width", function(d) { return x(0); })
                 .transition()
                 .duration(1500)
@@ -127,25 +162,40 @@ export function initChart(iframe) {
 
 function wrap(text, width) {
     text.each(function() {
-        var text = d3.select(this),
+        let text = d3.select(this),
             words = text.text().split(/\s+/).reverse(),
             word,
             line = [],
             y = text.attr("y"),
-            dy = words.length <= 3 ? 0.35 : words[0] == 'unida' ? 0.35 : -0.05,
+            dy = words.length == 2 && words[1] != 'Ayudar' ? 0.35 : words[0] == 'padres' ? -0.5 : -0.25,
             tspan = text.text(null).append("tspan").attr("x", -10).attr("y", y).attr("dy", + dy + "em");
 
-        console.log(words);
-
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
+        if(words.length == 8) {
+            let lines = 0.5;
+            while (word = words.pop()) {                
+                line.push(word);
                 tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan").attr("x", -10).attr("y", y).attr("dy", 0.8 + "em").text(word);
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", -10).attr("y", y).attr("dy", lines + "em").text(word);
+                    lines += 1;
+                }
+            }
+        } else {
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", -10).attr("y", y).attr("dy", 1 + "em").text(word);
+                }
             }
         }
+
+        
     });
 };
